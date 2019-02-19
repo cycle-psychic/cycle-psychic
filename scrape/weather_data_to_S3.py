40f74ebf4e256f365360192d9518302ae925bc92
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-02-19 18:01:09 +0000

    Added weather_data_to_S3.py script
    Added weather_data_to_S3.py script for moving files from EC2 to S3.
    Also removed space from the name of scraped_data_to_S3.py

---
+++ scrape/weather_data_to_S3.py
@@ -0,0 +1,13 @@
+# Used to move any files that have been scraped without being sent to S3
+
+import boto3
+import os
+
+s3_resource = boto3.resource('s3')
+
+# data is the folder containing the files to upload to s3
+for name in os.listdir('weather'):
+    print(name)
+    # Replace 'cycle-psyic' with your bucket name
+    # name is the name your file will be called in s3
+    s3_resource.Object('cycle-psychic-weather', name).upload_file(Filename='weather/'+name)
