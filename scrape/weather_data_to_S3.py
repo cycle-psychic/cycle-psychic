0f96016361e9cfe9a39f1c8189749feb8c0501f9
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-02-19 21:31:53 +0000

    Update weather_data_to_S3.py

--- scrape/weather_data_to_S3.py
+++ scrape/weather_data_to_S3.py
@@ -4,7 +4,7 @@ import boto3
 import os
 
 #create resource using aws credentials
-s3_resource = boto3.resource('s3',aws_access_key_id='AKIAIM7ICSBKEMDK5JXA', aws_secret_access_key='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')
+s3_resource = boto3.resource('s3',aws_access_key_id='12345678', aws_secret_access_key='12345678')
 
 # data is the folder containing the files to upload to s3
 for name in os.listdir('weather'):
