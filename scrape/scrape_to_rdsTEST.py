28d7ae7747e4b5f9aa3c3551da5f20106b31bd0e
NiamhOF <oflaherty.niamh@gmail.com>
2019-02-21 08:44:45 +0000

    change to mysql connector and ignore duplicate additions to table

--- scrape/scrape_to_rdsTEST.py
+++ scrape/scrape_to_rdsTEST.py
@@ -1,23 +1,10 @@
-
-import datetime
+from datetime import datetime
 import json
 import requests
 import boto3
-import pymysql.cursors
+import mysql.connector
 
-# def stations_to_db(bike_stations):
-#   for station in bike_stations:
-#     vals = (station.get('address'), station.get('contract_name'), \
-#       station.get('name'), station.get('last_update'), station.get('lng'), \
-#         station.get('lat'), station.get('status'), station.get('available_bikes'), \
-#           station.get('bonus'), station.get('available_bike_stands'),\
-#             station.get('number'), station.get('bike_stands'), station.get('banking'))
-#     return vals
-
-#  sql = "INSERT INTO `all_station_info` (`address`, `contract_name`, `name`, `last_update`, `lng`, `lat`,`status`, `available_bikes`, `bonus`, `available_bike_stands`, `number`, `bike_stands`, `banking`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", vals
-
-
-ts = str(datetime.datetime.now()).split('.')[0].replace(" ", "")
+ts = str(datetime.now()).split('.')[0].replace(" ", "")
 print("Starting scrape: " + ts)
 # retrieve bike stations
 api_key= "9cc2d222a1a4c269a200b0e146162e815f266332"
@@ -25,35 +12,28 @@ api_endpoint = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=
 r = requests.get(url=api_endpoint)
 bike_stations = r.json()
 print(bike_stations[0])
-# store bike staions to file locally
-#path='/home/ubuntu/src/dummyapp/data/'
-file_name=ts+'.json'
-#with open(path+file_name, 'w') as outfile:
-#    json.dump(bike_stations, outfile)
-# store bike stations to S3
-#s3_resource = boto3.resource('s3') 
-@@@  
 
-#s3_resource.Object('cycle-psychic', file_name).upload_file(Filename=path+file_name)
+
+file_name=ts+'.json'
 
 # store bike stations to RDS
-connection = pymysql.connect(
+connection = mysql.connector.connect(
   host='cyclepsychic.c7jha7i6ueuc.eu-west-1.rds.amazonaws.com',
   user='cyclepsychic',
   password='CyclePsychic123',
-  db='cyclepsychic',
-  autocommit=True)
+  db='cyclepsychic')
 
-with connection.cursor() as cursor:
-  for station in bike_stations:
-    vals = (station.get('address'), station.get('contract_name'), \
-      station.get('name'), station.get('last_update'), station.get('lng'), \
-        station.get('lat'), station.get('status'), station.get('available_bikes'), \
-          station.get('bonus'), station.get('available_bike_stands'),\
-            station.get('number'), station.get('bike_stands'), station.get('banking'))
-    sql = "INSERT INTO `all_station_info` (`address`, `contract_name`, `name`, `last_update`, `lng`, `lat`,`status`, `available_bikes`, `bonus`, `available_bike_stands`, `number`, `bike_stands`, `banking`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-    cursor.execute(sql, vals)
-  result = cursor.fetchone()
-  print(result)
+cursor = connection.cursor()
 
+for station in bike_stations:
+  vals = (station.get('address'), station.get('contract_name'), \
+    station.get('name'), datetime.fromtimestamp(int(station.get('last_update'))/1000), station['position'].get('lng'), \
+      station['position'].get('lat'), station.get('status'), station.get('available_bikes'), \
+        station.get('bonus'), station.get('available_bike_stands'),\
+          station.get('number'), station.get('bike_stands'), station.get('banking'))
+  sql = "INSERT IGNORE INTO `all_station_info` (`address`, `contract_name`, `name`, `last_update`, `lng`, `lat`,`status`, `available_bikes`, `bonus`, `available_bike_stands`, `number`, `bike_stands`, `banking`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+  cursor.execute(sql, vals)
+
+connection.commit()
 connection.close()
+                                             
