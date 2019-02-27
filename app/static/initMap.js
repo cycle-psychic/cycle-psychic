0b7bee3d9a79c2bb468b204286a760677753ff68
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-02-27 21:38:16 +0000

    Updated initMap.js to place markers for all stations
    Updated the script to connect to the Dublin Bikes API. The script then loops through all stations and places markers.

--- app/static/initMap.js
+++ app/static/initMap.js
@@ -6,14 +6,28 @@ function initMap() {
     // create the map
     map = new google.maps.Map(document.getElementById('map'), {
     // map will be centred on these co-ordinates when it loads
-    center: {lat: 53.3453, lng: -6.2722},
+    center: {lat: 53.347, lng: -6.268},
     // initial level of zoom when map loads - 15 is street level
-    zoom: 14,
+    zoom: 13.5,
     mapTypeControl: false
     });
 
-    var testmarker = new google.maps.Marker({
-        position: {lat: 53.3453, lng: -6.2722}, //marker position
-        map: map, //map that the marker will be placed on
+    // call the Dublin Bikes API directly using JQuery
+    $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=fd4562884252e255617667387120a3a9ea10a259", null, function(data) {
+
+        // add markers to the map - loop through each station in the JSON object
+        for (var i = 0; i < data.length; i++) {
+            
+            // get the latitude and longitude for the station
+            var latitude = data[i].position.lat;
+            var longitude = data[i].position.lng;
+            var latLng = new google.maps.LatLng(latitude, longitude);
+
+            // generate the marker for the station and place on the map
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+                });
+            };
         });
 }
