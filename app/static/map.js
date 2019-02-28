469e5496bec90525fd362a7fc244865d71e12fc0
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-02-28 19:03:55 +0000

    Added initial logic for marker icons
    Added initial logic for marker icons - takes bike occupancy & payment availability into account.
    Icons are currently placeholder images.

--- app/static/map.js
+++ app/static/map.js
@@ -17,20 +17,60 @@ function initMap() {
 
     // call the Dublin Bikes API directly using JQuery
     $.getJSON(urlBikes, null, function(data) {
-
-        // add markers to the map - loop through each station in the JSON object
-        for (var i = 0; i < data.length; i++) {
-            
-            // get the latitude and longitude for the station
-            var latitude = data[i].position.lat;
-            var longitude = data[i].position.lng;
-            var latLng = new google.maps.LatLng(latitude, longitude);
-
-            // generate the marker for the station and place on the map
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-                });
-            };
-        });
+        addMarkers(data);
+    });
 }
+
+//function for adding markers to the map
+function addMarkers(data) {
+    // add markers to the map - loop through each station in the JSON object
+    for (var i = 0; i < data.length; i++) {
+        
+        // get the latitude and longitude for the station
+        var latitude = data[i].position.lat;
+        var longitude = data[i].position.lng;
+        var latLng = new google.maps.LatLng(latitude, longitude);
+
+        // get the occupancy info for each station
+        var totalStands = data[i].bike_stands;
+        var availableBikes = data[i].available_bikes;
+        // calculate the percentage of available bikes
+        var percentAvailable = (availableBikes/totalStands)*100;
+
+        //get payment info for each station
+        var cardPayments = data[i].banking;
+
+        // check which icon the marker should use based on percentage & payment types
+        if (cardPayments == true) {
+            if (percentAvailable >= 67) {
+                var urlIcon = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";  // use the red marker with euro symbol
+            }
+            else if (percentAvailable >= 33) {
+                var urlIcon = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";  // use the orange marker with euro symbol
+            }
+            else {
+                var urlIcon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";  // use the green marker with euro symbol
+            }
+        }
+        else {
+            if (percentAvailable >= 67) {
+                var urlIcon = "http://maps.google.com/mapfiles/ms/icons/green.png";  // use the red marker without euro symbol
+            }
+            else if (percentAvailable >= 33) {
+                var urlIcon = "http://maps.google.com/mapfiles/ms/icons/yellow.png";  // use the orange marker without euro symbol
+            }
+            else {
+                var urlIcon = "http://maps.google.com/mapfiles/ms/icons/red.png";  // use the green marker without euro symbol
+            }
+        }
+
+        // generate the marker for the station and place on the map
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon: {
+                url: urlIcon
+            }
+        });
+    };
+};
