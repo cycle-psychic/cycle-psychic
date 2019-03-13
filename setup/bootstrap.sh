cf9b793ee52d8602e7d19b7daf0efff29b2229a8
NiamhOF <oflaherty.niamh@gmail.com>
2019-03-13 21:55:28 +0000

    updated with requirements for data analytics

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -28,3 +28,12 @@ conda install -c anaconda mysql-connector-python
 # Install jsonify
 conda install jsonify
 
+# Install requirements for data analysis
+conda install pandas
+
+conda install numpy
+
+conda install matplotlib
+
+conda install seaborn
+
