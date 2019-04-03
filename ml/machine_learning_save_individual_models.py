e09ca1f96ed77eb17d0270259878dc17d842374c
NiamhOF <oflaherty.niamh@gmail.com>
2019-04-03 21:54:01 +0100

    Script for saving a model per station

---
+++ ml/machine_learning_save_individual_models.py
@@ -0,0 +1,52 @@
+#Import pandas and scikitlearn for Machine Learning Models
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import BaggingRegressor
+
+# Pickle saves the model
+import pickle
+
+def split_by_position(features, targets):
+    """
+    train 0.80
+    test 0.20
+    """
+    len_train = int(0.80 * len(features))
+    train_features = features[0:len_train]
+    train_targets = targets[0:len_train]
+    test_features = features[len_train:]
+    test_targets = targets[len_train:]
+    return train_features, test_features, train_targets, test_targets
+
+df = pd.read_csv('./bike+weather_to_28_03_19.csv')
+
+
+def train_station(station_df):
+  features = df[['number', 'hour', 'minute',
+              'main_temp', 'main_wind_speed', 'main_rain_volume_1h', 'main_snow_volume_1h',
+              'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+              'Sunday', 'clouds', 'atmosphere', 'snow', 'light_rain',
+              'rain', 'light_drizzle', 'drizzle', 'thunderstorm']].values
+  targets = df['available_bikes'].values
+
+  train_features, test_features, train_targets, test_targets = split_by_position(features, targets)
+
+  scaler = StandardScaler()
+  scaler.fit(train_features)
+  scaled_train_features = scaler.transform(train_features)
+
+  estimator = BaggingRegressor()
+  estimator.fit(scaled_train_features, train_targets)
+
+  return scaler, estimator
+
+
+# Cycle through station numbers 2 to 115
+for station in range (2,116):
+  print('Training station: ' , str(station))
+  scaler, estimator = train_station(df.loc[df['number'] == station])
+  print('Saving model and scaler for station: ' , str(station))
+  filename_est = '../app/models/model'+str(station)+'.sav'
+  filename_scaler = '../app/models/scaler'+str(station)+'.sav'
+  pickle.dump(estimator, open(filename_est, 'wb'))
+  pickle.dump(scaler, open(filename_scaler, 'wb'))
