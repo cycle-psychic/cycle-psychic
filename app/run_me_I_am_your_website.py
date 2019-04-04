92360aa948f4869dd8c1483af0248f8b7b233332
NiamhOF <oflaherty.niamh@gmail.com>
2019-04-04 10:34:56 +0100

    import pickle

--- app/run_me_I_am_your_website.py
+++ app/run_me_I_am_your_website.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify
 import requests
 import mysql.connector
 from sklearn.preprocessing import StandardScaler
+import pickle
 import math
 
 import datetime
