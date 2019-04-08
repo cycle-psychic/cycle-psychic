e7327fa8ef3bcf3617856839e16d3c51d96906d3
Karim <karim.elgendy2@mail.dcu.ie>
2019-04-08 19:02:32 +0100

    Added function to get available bikes by hour for the previous week

--- app/run_me_I_am_your_website.py
+++ app/run_me_I_am_your_website.py
@@ -4,8 +4,8 @@ import mysql.connector
 from sklearn.preprocessing import StandardScaler
 import pickle
 import math
-
 import datetime
+from datetime import timedelta
 
 app = Flask(__name__)
 
@@ -138,6 +138,46 @@ def avgChartData(station_address):
         averageByHour[hour] = int(round(row[0]))
 
     return jsonify(averageByHour)
+
+@app.route('/bikes1week/<station_address>')
+def bikes_available_1week(station_address):
+
+    # get current date
+    date = datetime.datetime.now()
+    # remove trailing minutes (to check for available bikes during this hour)
+    date = date.replace(minute=0,second=0,microsecond=0)
+    # convert time to hours only
+    hour = date.strftime("%X")
+
+    # add 59 minutes and 59 seconds for SQL query and format it correctly
+    datePlusOneHour = date + timedelta(hours=1) - timedelta(seconds=1)
+    datePlusOneHour = str(datePlusOneHour)
+    datePlusOneHour = datePlusOneHour.split(" ")[1]
+
+    # fix the URL request for a specific station
+    station_address = station_address
+    station_address = station_address.replace("_"," ")
+
+    # build query
+    static_query1 = """ SELECT avg(available_bikes), last_update as Weekday
+                FROM all_station_info 
+                WHERE cast(last_update as time) between \'"""
+    static_query2 = hour + "\' and \'" + str(datePlusOneHour)
+    static_query3 = "\' AND address=\'" + station_address
+    static_query4 = "\' AND YEARWEEK(last_update) = yearweek(NOW() - INTERVAL 1 WEEK) Group by Weekday(last_update);"
+    constructedQuery = static_query1 + static_query2 + static_query3 + static_query4
+
+    # Holds average bikes organised by hour
+    daily_available_bikes = {}
+
+    # execute
+    rows = open_connection(constructedQuery)
+
+    for row in rows:
+        weekday =  row[1].strftime('%a')
+        daily_available_bikes[weekday] = round(row[0])
+
+    return jsonify(daily_available_bikes)
 
 def predict(station_id, time_date):
     # Required fields: number, hour, minute, main_temp, main_wind_speed,
